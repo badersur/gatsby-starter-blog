95d44c9c082857f98fe5c6a64f20bd38263deaa6
Bader Nasser <badersur@gmail.com>
2020-01-31 23:17:20 +0400

    Use TypeScript in Gatsby's API Files

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -1,3 +1,9 @@
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/browser-apis/
+ */
+
 // custom typefaces
 import "typeface-montserrat";
 import "typeface-merriweather";
--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,3 +1,9 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
 /* eslint-disable import/prefer-default-export */
 export { createPages } from "./scripts/create-pages";
 export { onCreateNode } from "./scripts/on-create-node";
